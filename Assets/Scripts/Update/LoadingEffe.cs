3af4869a15b54dedb6cc7814d10facbc0a1033c6
Nikos Karantakis <nick.karantakis@gmail.com>
2020-05-05 20:14:10 +0300

    Commit Changes

---
+++ Assets/Scripts/Update/LoadingEffe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingEffe : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(SleepMethod());
+    }
+
+    IEnumerator SleepMethod() {
+        while (true)
+        {
+            gameObject.transform.Rotate(Vector3.back, 30f);
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+}
